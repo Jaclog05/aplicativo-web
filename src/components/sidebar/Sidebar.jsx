917f3c7f99bde8c95dca3f211a8b9818011e29e6
Jaider <jaidercueto.1010@gmail.com>
2025-03-28 16:40:06 -0500

    Adding logout confirmation in sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,16 +6,20 @@ function Sidebar({ onClick, options }) {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    sessionStorage.removeItem("appraisalToken");
-    sessionStorage.removeItem("appraisalsData");
-    sessionStorage.removeItem("squareMPrices");
-    navigate("/admin-login");
+    const confirmLogout = window.confirm(
+      "¿Estás seguro de que deseas cerrar sesión?"
+    );
+
+    if (confirmLogout) {
+      sessionStorage.removeItem("appraisalToken");
+      sessionStorage.removeItem("appraisalsData");
+      sessionStorage.removeItem("squareMPrices");
+      navigate("/admin-login");
+    }
   };
 
   return (
-    <aside
-      className="h-md-100 p-md-2 d-md-flex flex-md-column col-md-2 bg-light"
-    >
+    <aside className="h-md-100 p-md-2 d-md-flex flex-md-column col-md-2 bg-light">
       <div className="text-center mb-md-2">
         <img
           src={adminProfileIcon}
@@ -23,22 +27,35 @@ function Sidebar({ onClick, options }) {
           className="d-none d-md-block mx-auto w-75 text-dark"
         />
         <p className="mb-0 fw-bold">Admin 1</p>
-        <p className="mb-0 fst-italic text-break px-2" style={{fontSize: '0.8rem'}}>admin1@example.com</p>
+        <p
+          className="mb-0 fst-italic text-break px-2"
+          style={{ fontSize: "0.8rem" }}
+        >
+          admin1@example.com
+        </p>
       </div>
 
-      <hr className="d-none d-md-block"/>
+      <hr className="d-none d-md-block" />
 
-      <p className="d-none d-md-block my-md-2" style={{fontSize: '16px', fontWeight: '900px' }}>Opciones</p>
+      <p
+        className="d-none d-md-block my-md-2"
+        style={{ fontSize: "16px", fontWeight: "900px" }}
+      >
+        Opciones
+      </p>
 
       <nav className="d-flex align-items-center justify-content-center">
-        <ul className="w-100 p-0 mb-0 list-unstyled list-inline d-flex d-md-block justify-content-around" style={{fontSize: '14px'}}>
+        <ul
+          className="w-100 p-0 mb-0 list-unstyled list-inline d-flex d-md-block justify-content-around"
+          style={{ fontSize: "14px" }}
+        >
           <li className="inline-item text-md-left mx-md-2">
             <a
               name="list"
               value={options[0]}
               onClick={onClick}
               className="text-dark text-decoration-none d-block p-2 nav-link-sidebar-hover"
-              style={options[0] ? {background: '#8B8B8B', color: '#FFF'} : {}}
+              style={options[0] ? { background: "#8B8B8B", color: "#FFF" } : {}}
             >
               Registros del aplicativo web
             </a>
@@ -49,13 +66,18 @@ function Sidebar({ onClick, options }) {
               value={options[1]}
               onClick={onClick}
               className="text-dark text-decoration-none d-block p-2 nav-link-sidebar-hover"
-              style={options[1] ? {background: '#8B8B8B', color: '#FFF'} : {}}
+              style={options[1] ? { background: "#8B8B8B", color: "#FFF" } : {}}
             >
               Editar precio Metro cuadrado
             </a>
           </li>
           <li className="inline-item text-md-left mx-md-2 p-2 nav-link-sidebar-hover">
-            <a onClick={handleLogout}>Cerrar Sesión</a>
+            <a
+              onClick={handleLogout}
+              className="text-danger text-decoration-none"
+            >
+              Cerrar Sesión
+            </a>
           </li>
         </ul>
       </nav>
